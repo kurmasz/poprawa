99c98a8680a34e971768e3945ce84126f7564a04
Anna Carvalho <ajcarvalho1201@gmail.com>
2022-11-29 18:08:42 -0500

    now displays total mark amounts

--- lib/report_generator.rb
+++ lib/report_generator.rb
@@ -34,7 +34,6 @@ class ReportGenerator
   #
   def self.generate_report(student, gradebook, out)
     out.puts "# Progress Report for #{student.full_name}"
-    out.puts
 
     out.puts <<HERE
     Note:  This is a draft of the progress report generator.  This version only shows the
@@ -44,18 +43,18 @@ HERE
 
     gradebook.categories.each do |category|
       out.puts "## #{category[:title]}"
-      out.puts
       out.puts "|#{category[:title]}|Grade|Late Days|"
       out.puts "|------|-------|-------|"
 
       category[:assignment_names].each do |key, value|
-        mark = format_marks(student.get_mark(category[:key], key))
+        marks = format_marks(student.get_mark(category[:key], key))
         late_days = student.get_late_days(category[:key], key)
 
-        out.printf "|%s (%s)|%s|%s|\n", value, key, mark, late_days
+        out.printf "|%s (%s)|%s|%s|\n", value, key, marks, late_days
       end # each item
-      out.puts
-      out.puts
+
+      generate_mark_breakdown(student, category, out)
+
     end # each category
 
     generate_legend(out)
@@ -64,9 +63,9 @@ HERE
   end
 
   #
-  # format_grades
+  # format_marks
   #
-  # returns a formatted string of grades with the highest grade bolded
+  # returns a formatted string of marks with the highest grade bolded
   #
   def self.format_marks(marks)
     return if marks.nil?
@@ -91,18 +90,45 @@ HERE
     return marks.join(' ')
   end
 
-  def self.generate_legend(out)
+  #
+  # generate_mark_breakdown
+  #
+  def self.generate_mark_breakdown(student, category, out)
+    numE = 0
+    numM = 0
+    numP = 0
+    numX = 0
 
-      out.puts
-      out.puts "## Legend "
-      out.puts "* `e`: Exceeds expectations"
-      out.puts "* `m`: Meets expectations"
-      out.puts "* `p`: Progressing"
-      out.puts "* `x`: Not Yet"
-      out.puts "* `.`: Missing"
-      out.puts "* `d`: Demonstrated but not yet graded"
-      out.puts "* `r`: Received but not yet graded"
-      out.puts "* `?`: Received; Grading in progress"
-      out.puts "* `!`: Error in gradesheet"
+    category[:assignment_names].each do |key, value|
+      marks = student.get_mark(category[:key], key)
+      
+      next if marks.nil?
+
+      marks = marks.split('')
+      
+      numE += marks.count('e')
+      numM += marks.count('m')
+      numP += marks.count('p')
+      numX += marks.count('x') + marks.count('?') + marks.count('.')
+    end
+
+    out.puts
+    out.puts "|E|M|P|X|"
+    out.puts "|------|-------|-------|-------|"
+    out.puts "|#{numE}|#{numM}|#{numP}|#{numX}|"
+  end
+
+  def self.generate_legend(out)
+    out.puts
+    out.puts "## Legend "
+    out.puts "* `e`: Exceeds expectations"
+    out.puts "* `m`: Meets expectations"
+    out.puts "* `p`: Progressing"
+    out.puts "* `x`: Not Yet"
+    out.puts "* `.`: Missing"
+    out.puts "* `d`: Demonstrated but not yet graded"
+    out.puts "* `r`: Received but not yet graded"
+    out.puts "* `?`: Received; Grading in progress"
+    out.puts "* `!`: Error in gradesheet"
   end
 end
