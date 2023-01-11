4f676dd0610ff623240fbd18ccb647d95a9a5643
Anna Carvalho <ajcarvalho1201@gmail.com>
2023-01-11 10:55:43 -0500

    moved path hardcoding from generate_graph to report_generator

--- lib/poprawa/report_generator.rb
+++ lib/poprawa/report_generator.rb
@@ -125,7 +125,9 @@ HERE
       out.puts
       out.puts "#{mark_count[:e] + mark_count[:m]} at 'm' or better."
 
-      system("node lib/generate_graph.js #{student.info[:github]} #{category[:title].delete(" ")} #{mark_count[:m] + mark_count[:e]} #{assigned}")
+      # but now it's hard-coded here
+      path = "demo/progressReports/#{student.info[:github]}/#{category[:title].delete(" ")}.png"
+      system("node lib/generate_graph.js #{path} #{category[:title].delete(" ")} #{mark_count[:m] + mark_count[:e]} #{assigned}")
 
       out.puts
       out.puts "![#{category[:title]}](#{category[:title].delete(" ")}.png)"
